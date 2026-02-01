a508f058e835bf7b1841ef99cd3908743ed5c87b
Your Name <your@email.com>
2026-02-01 17:50:51 +0530

    Update message to trigger Jenkins

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Maven Java Application");
+        System.out.println("Hello from Jenkins Build #2");
     }
 }
 
